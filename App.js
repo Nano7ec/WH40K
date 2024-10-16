961abf9951255058f49a07237bd2a8148fadc57e
Nano7ec <125903775+Nano7ec@users.noreply.github.com>
2024-10-16 09:13:19 -0600

    necrones agregados
    solo las reglas de army aun no inicioo el apartado de figuras

--- App.js
+++ App.js
@@ -7,6 +7,11 @@ import ArmyTyranidScreen from './src/screens/ArmyTyranid';
 // Importar las nuevas pantallas
 import TyranidsScreen from './src/screens/Tyranids';
 import MinTyraScreen from './src/screens/minTyra';
+// importar los necrones
+import ArmyNecronsScreen  from './src/screens/ArmyNecrons';
+import NecronsScreen from './src/screens/Necrons';
+import MinNecronsScreen from './src/screens/minNecrons';
+
 
 const Stack = createNativeStackNavigator();
 
@@ -20,6 +25,10 @@ export default function App() {
          {/* Definir las nuevas pantallas */}
          <Stack.Screen name="Tyranids" component={TyranidsScreen} options={{ title: 'Tyranids' }} />
         <Stack.Screen name="minTyra" component={MinTyraScreen} options={{ title: 'Min Tyra' }} />
+        {/**Necrones */}
+        <Stack.Screen name="ArmyNecrons" component={ArmyNecronsScreen} options={{ title: 'Army Necrons' }} />
+        <Stack.Screen name="Necrons" component={NecronsScreen} options={{ title: 'Necrons' }} />
+        <Stack.Screen name="minNecrons" component={MinNecronsScreen} options={{ title: 'Min Necrons' }} />
       
       </Stack.Navigator>
     </NavigationContainer>
